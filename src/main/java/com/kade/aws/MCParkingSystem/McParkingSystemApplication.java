d6342e5089ea00a890771285c6917f3f1a6e2f9e
KadeHennacy <kade.hennacy@gmail.com>
2022-08-16 17:38:05 -0400

    Initial commit

---
+++ src/main/java/com/kade/aws/MCParkingSystem/McParkingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kade.aws.MCParkingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McParkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McParkingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kade/aws/MCParkingSystem/McParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kade.aws.MCParkingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
