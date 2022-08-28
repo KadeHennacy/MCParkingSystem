6a0a787e2cc4731a247b3a073c36861b2a993917
KadeHennacy <kade.hennacy@gmail.com>
2022-08-28 16:18:28 -0400

    Changed HelloController.java

--- src/main/java/com/kade/aws/MCParkingSystem/HelloController.java
+++ src/main/java/com/kade/aws/MCParkingSystem/HelloController.java
@@ -1,15 +1,16 @@
 package com.kade.aws.MCParkingSystem;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/hello")
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @GetMapping
+    public String hello() {
+        return "Hello TechPrimers";
     }
-
 }
 
