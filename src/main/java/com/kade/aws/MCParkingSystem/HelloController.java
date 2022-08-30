f0094b4b8808d702e76bd9a87aa5073767a7882c
KadeHennacy <kade.hennacy@gmail.com>
2022-08-30 11:39:49 -0400

    Changed text on homepage

--- src/main/java/com/kade/aws/MCParkingSystem/HelloController.java
+++ src/main/java/com/kade/aws/MCParkingSystem/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "This is the develop branch!";
+        return "This is the develop branch!!";
     }
 }
 
